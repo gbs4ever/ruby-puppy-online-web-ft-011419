3fc3b99531ae04034b365a1c58368085e424904e
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-24 03:52:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,17 @@
 class Dog
 @@all=[]
-def initialize(name)
-@name= name
-@@all << self
-end
+
+  def initialize(name)
+  @name= name
+  @@all << self
+  end
+
+
+  def self.all
+    @@all.each do |dog_name|
+      puts "#{dog_name}"
+    
+  end
 
 
 
